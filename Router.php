2b10f2df3d4ab1055c2fde8da03e1cf013e3999d
Thiago Henrique <thiago.h.silva.98@gmail.com>
2019-04-03 11:20:16 -0400

    Router class

--- Router.php
+++ Router.php
@@ -1,5 +1,71 @@
 <?php  
 
 class Router {
-    //put your code here
+    
+	private $request;
+	private $supportedHttpMethod = array(
+		"GET",
+		"POST"
+	);
+
+	function __construct(IResquest $request)
+	{
+		$this->request = $request;
+	}
+	
+	function __call($name,$args)
+	{
+		list($route,$method) = $args;
+		
+		if(!in_array(strtoupper($name),$this->supportedHttpMethod))
+		{
+			$this->invalidMethodHandler();
+		}
+		
+		$this->{strtolower($name)}[$this->formatRoute($route)] = $method ;
+	}
+	
+	private function formatRoute($route)
+	{
+		$result = rtrim($route,'/');
+		
+		if($result == '')
+		{
+			return '/';
+		}
+		
+		return $result ;
+	}
+	
+	private function invalidMethodHandler(){
+		
+		header("{$this->request->serverProtocol} 405 Method Not Allowed");
+	
+	}
+	
+	private function defaultRequestHandler(){
+			
+	    header("{$this->request->serverProtocol} 404 Not Found");
+
+	}
+	
+	function resolve()
+	{
+		
+		$methodDictionary = $this->{strtolower($this->request->requestMethod)};
+		$formatedRoute = $this->formatRoute($this->request->requestUri);
+		$method = $methodDictionary[$formatedRoute];
+		
+		if(is_null($method)){
+			$this->defaultRequestHandler();
+			return;	
+		}
+		
+		echo call_user_func_array($method,array($this-request));
+	}
+	
+	function __destruct()
+	{
+		$this->resolve();
+	}
 }
