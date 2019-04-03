23dcf90357971cf4518f60efa71844afc6131078
Thiago Henrique <thiago.h.silva.98@gmail.com>
2019-04-03 13:32:17 -0400

    Revert "teste1"
    This reverts commit b4b44e105253eb1e37386bb8369a029b45c638d7.

--- public/index.php
+++ public/index.php
@@ -1,14 +1,12 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-include_once 'Request.php';
-include_once 'Router.php'
+//include_once 'Request.php';
+//include_once 'Router.php'
 
-$router = new Router(new Request);
+//$router = new Router(new Request);
 
-$router->get('/',function(){
-	return "rota raiz";
-});
+//$router->get('/',function(){
+	//return "rota raiz";
+//});
 
+echo "teste";
