3bd9e7584b1d968edc9f2358ba57cde71ce685cd
Thiago Henrique <thiago.h.silva.98@gmail.com>
2019-04-03 11:40:39 -0400

    teste

--- public/index.php
+++ public/index.php
@@ -1,10 +1,12 @@
 <?php
 
-include_once 'Request.php';
-include_once 'Router.php'
+//include_once 'Request.php';
+//include_once 'Router.php'
 
-$router = new Router(new Request);
+//$router = new Router(new Request);
 
-$router->get('/',function(){
-	return "rota raiz";
-});
+//$router->get('/',function(){
+	//return "rota raiz";
+//});
+
+echo "teste";
